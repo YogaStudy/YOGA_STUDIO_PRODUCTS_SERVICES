7dbacfee2a9cd518ff65409ef38e4c786f3c38af
NavMoudgill <navbhartimoudgill123@gmail.com>
2024-04-09 19:09:38 +0200

    second

--- components/app/index.tsx
+++ components/app/index.tsx
@@ -3,30 +3,25 @@ import Copyright from "@components/Copyright";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
 import { ToastContainer } from "react-toastify";
-import dynamic from "next/dynamic";
 interface MainProps {
   childrenElement: React.ReactNode;
 }
-const ProviderSSR = dynamic(() => import("@components/Provider"), {
-  ssr: false,
-});
+
 const Main = ({ childrenElement }: MainProps) => {
   return (
     <>
       <ToastContainer />
-      <ProviderSSR>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app min-h-[calc(100vh-300px)] ">
-          <Navbar />
-          {childrenElement}
-        </main>
-        <footer>
-          <Footer />
-          <Copyright />
-        </footer>
-      </ProviderSSR>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <main className="app min-h-[calc(100vh-300px)] ">
+        <Navbar />
+        {childrenElement}
+      </main>
+      <footer>
+        <Footer />
+        <Copyright />
+      </footer>
     </>
   );
 };
